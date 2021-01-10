d00a014e227412ba5c00ae3c9e47135f7fe16de4
ykanno0521 <54009743+ykanno0521@users.noreply.github.com>
2021-01-11 00:08:31 +0900

    map, filterを使った配列

--- src/index.js
+++ src/index.js
@@ -145,3 +145,35 @@
 
 // const arr7 = [...arr4, ...arr5];
 // console.log(arr7);
+
+/**
+ * map, filterを使った配列
+ */
+// const nameArr = ["田中", "鈴木", "山田"];
+// for (let index = 0; index < nameArr.length; index++){
+//   console.log(`${index}番目は${nameArr[index]}`);
+// }
+
+// const nameArr2 = nameArr.map((name)=>{
+//   return name;
+// })
+// console.log(nameArr2);
+
+// nameArr.map((name, index)=> console.log(`${index + 1}番目は${name}です。`));
+
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 1;
+// });
+
+// console.log(newNumArr);
+
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "山田") {
+//     return name
+//   } else {
+//     return `${name}さん`
+//   }
+// })
+
+// console.log(newNameArr);

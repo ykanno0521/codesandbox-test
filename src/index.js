f1ea2281ca510054c68f5611fcb05dcd817abb88
ykanno0521 <54009743+ykanno0521@users.noreply.github.com>
2021-01-10 23:49:23 +0900

    配列のコピー、結合

--- src/index.js
+++ src/index.js
@@ -114,3 +114,34 @@
 // const sayHello2 = (name = "じゃけぇ") => console.log(`こんにちは!${name}`);
 // sayHello2();
 // =>こんにちは!じゃけぇ
+
+/**
+ * スプレッド構文 ...
+ */
+
+//  配列の展開
+// const arr1 = [1, 2];
+// // console.log(arr1);
+// // console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// // sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+// まとめる
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+// 配列のコピー、結合
+// const arr4 = [10, 40];
+// const arr5 = [20, 50];
+
+// const arr6 = [...arr4];
+// // const arr6 = arr4　描いてしまうと参照元にも影響が出る。
+// console.log(arr6);
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
